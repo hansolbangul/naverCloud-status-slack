27712df3131af97eb68db2bcc0eedd4be166d7f5
hansolbangul <hansolbangul@farmos.co.kr>
2022-03-25 16:28:04 +0900

    test

--- status.js
+++ status.js
@@ -23,6 +23,7 @@ const status = async () => {
     console.log(cpStatus)
 
     console.log(stStatus.status === false ? cpStatus.status === false ? '2가지 에러사항' : '1가지 에러사항' : cpStatus.status === true ? false : '1가지 에러사항')
+    console.log(stStatus.map(e => ({message: e.message, subtitle: e.subtitle})))
     console.log(stStatus.status === false ? cpStatus.status === false ? stStatus.map(e => ({message: e.message, subtitle: e.subtitle})).concat(cpStatus.map(e => ({message: e.message, subtitle: e.subtitle}))) : stStatus.map(e => ({message: e.message, subtitle: e.subtitle})) : cpStatus.status === true ? false : cpStatus.map(e => ({message: e.message, subtitle: e.subtitle})))
 
     const message = {
